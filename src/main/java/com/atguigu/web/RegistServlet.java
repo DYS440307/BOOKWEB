eda3e9a52793cecef528d046ce12d79ea06a7c42
dys440307 <dys440307@outlook.com>
2023-02-07 11:17:20 +0800

    自己的垃圾2

--- src/main/java/com/atguigu/web/RegistServlet.java
+++ src/main/java/com/atguigu/web/RegistServlet.java
@@ -10,43 +10,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * dys
- * 2023/2/6 10:08
- *
- * @version 1.0
- */
 public class RegistServlet extends HttpServlet {
+
     private UserService userService = new UserServiceImpl();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-//        1、获取请求的参数,写的是表单项的name值
+        //  1、获取请求的参数
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
         String code = req.getParameter("code");
-//        2、检查验证码是否正确正确--写死了
-        if ("adcde".equalsIgnoreCase(code)) {
 
-//        3、检查用户名是否可用可用
+//        2、检查 验证码是否正确  === 写死,要求验证码为:abcde
+        if ("abcde".equalsIgnoreCase(code)) {
+//        3、检查 用户名是否可用
             if (userService.existsUsername(username)) {
-                System.out.println("用户名字" + username + "已经存在不可用");
+                System.out.println("用户名[" + username + "]已存在!");
+//        跳回注册页面
                 req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
             } else {
+                //      可用
+//                调用Sservice保存到数据库
                 userService.registerUser(new User(null, username, password, email));
+//
+//        跳到注册成功页面 regist_success.html
                 req.getRequestDispatcher("/pages/user/regist_success.html").forward(req, resp);
-
             }
-//        调用Service保存到数据库
-//        跳到注册成功regist_success.html
-//        不可用跳回注册页面
         } else {
-//        不正确跳回注册页面
-            System.out.println("验证码错误");
+            System.out.println("验证码[" + code + "]错误");
             req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
         }
-
     }
 }
