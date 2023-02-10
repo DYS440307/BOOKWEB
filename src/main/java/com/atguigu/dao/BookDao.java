53875239b4a99b252204178e71a8043abc441709
dys440307 <dys440307@outlook.com>
2023-02-10 16:59:39 +0800

    修改后测试完成

--- src/main/java/com/atguigu/dao/BookDao.java
+++ src/main/java/com/atguigu/dao/BookDao.java
@@ -4,22 +4,16 @@ import com.atguigu.pojo.Book;
 
 import java.util.List;
 
-/**
- * dys
- * 2023/2/10 16:00
- *
- * @version 1.0
- */
 public interface BookDao {
+
     public int addBook(Book book);
 
     public int deleteBookById(Integer id);
 
     public int updateBook(Book book);
 
-    public Book QueryBookById(Integer id);
+    public Book queryBookById(Integer id);
 
     public List<Book> queryBooks();
-
 
 }
--- src/main/java/com/atguigu/dao/impl/BookDaoImpl.java
+++ src/main/java/com/atguigu/dao/impl/BookDaoImpl.java
@@ -5,22 +5,19 @@ import com.atguigu.pojo.Book;
 
 import java.util.List;
 
-/**
- * dys
- * 2023/2/10 16:17
- *
- * @version 1.0
- */
 public class BookDaoImpl extends BaseDao implements BookDao {
     @Override
     public int addBook(Book book) {
-        String sql = "insert into t_book(`id` , `name` , `author` , `price` , `sales` , `stock` , `img_path`) values(?,?,?,?,?,?,?)";
+
+        String sql = "insert into t_book(`name`,`author`,`price`,`sales`,`stock`,`img_path`) values(?,?,?,?,?,?)";
+
         return update(sql , book.getName() , book.getAuthor() , book.getPrice() , book.getSales() , book.getStock() , book.getImgPath());
+
     }
 
     @Override
     public int deleteBookById(Integer id) {
-        String sql = "delete from t_book where id=?";
+        String sql = "delete from t_book where id = ?";
         return update(sql , id);
     }
 
@@ -31,7 +28,7 @@ public class BookDaoImpl extends BaseDao implements BookDao {
     }
 
     @Override
-    public Book QueryBookById(Integer id) {
+    public Book queryBookById(Integer id) {
         String sql = "select `id` , `name` , `author` , `price` , `sales` , `stock` , `img_path` imgPath from t_book where id = ?";
         return queryForOne(Book.class , sql , id);
     }
