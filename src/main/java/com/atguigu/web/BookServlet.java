610759da8bed85ac26da6607ddd882e416ad4bc0
dys440307 <dys440307@outlook.com>
2023-02-10 23:01:04 +0800

    图书的模块

--- src/main/java/com/atguigu/web/BookServlet.java
+++ src/main/java/com/atguigu/web/BookServlet.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class BookServlet extends BaseServlet{
+public class BookServlet extends BaseServlet {
 
     private BookService bookService = new BookServiceImpl();
 
-    protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void add(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
 
         //        1、获取请求的参数==封装成为Book对象
-        Book book = WebUtils.copyParamToBean(req.getParameterMap(),new Book());
+        Book book = WebUtils.copyParamToBean(req.getParameterMap() , new Book());
 //        2、调用BookService.addBook()保存图书
         bookService.addBook(book);
 //        3、跳到图书列表页面
@@ -30,10 +30,9 @@ public class BookServlet extends BaseServlet{
     }
 
 
-
-    protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void delete(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
 //        1、获取请求的参数id，图书编程
-        int id = WebUtils.parseInt(req.getParameter("id"), 0);
+        int id = WebUtils.parseInt(req.getParameter("id") , 0);
 //        2、调用bookService.deleteBookById();删除图书
         bookService.deleteBookById(id);
 //        3、重定向回图书列表管理页面
@@ -42,10 +41,9 @@ public class BookServlet extends BaseServlet{
     }
 
 
-
-    protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void update(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
 //        1、获取请求的参数==封装成为Book对象
-        Book book = WebUtils.copyParamToBean(req.getParameterMap(),new Book());
+        Book book = WebUtils.copyParamToBean(req.getParameterMap() , new Book());
 //        2、调用BookService.updateBook( book );修改图书
         bookService.updateBook(book);
 //        3、重定向回图书列表管理页面
@@ -54,30 +52,26 @@ public class BookServlet extends BaseServlet{
     }
 
 
-
-    protected void getBook(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void getBook(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
         //1 获取请求的参数图书编号
-        int id = WebUtils.parseInt(req.getParameter("id"), 0);
+        int id = WebUtils.parseInt(req.getParameter("id") , 0);
         //2 调用bookService.queryBookById查询图书
         Book book = bookService.queryBookById(id);
         //3 保存到图书到Request域中
-        req.setAttribute("book", book) ;
+        req.setAttribute("book" , book);
         //4 请求转发到。pages/manager/book_edit.jsp页面
-        req.getRequestDispatcher("/pages/manager/book_edit.jsp").forward(req,resp);
+        req.getRequestDispatcher("/pages/manager/book_edit.jsp").forward(req , resp);
     }
 
 
-
-    protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void list(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
         //1 通过BookService查询全部图书
         List<Book> books = bookService.queryBooks();
         //2 把全部图书保存到Request域中
-        req.setAttribute("books", books);
+        req.setAttribute("books" , books);
         //3、请求转发到/pages/manager/book_manager.jsp页面
-        req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req,resp);
+        req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req , resp);
     }
-
-
 
 
 }
